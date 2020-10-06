0bfc0f5ebaa4ef9b8d7d569599847f395081d722
Leo Barbosa <leobarbosa.ti@gmail.com>
2020-10-06 16:58:08 -0300

    teste

--- Sistema_PetShop/Startup.cs
+++ Sistema_PetShop/Startup.cs
@@ -44,6 +44,8 @@ namespace Sistema_PetShop
             }
             else
             {
+
+                //teste
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
